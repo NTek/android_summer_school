1c91cbee232d906dad1dea334768c1ead7e10618
Milos Milanovic <milos.milanovic@rt-rk.com>
2014-06-19 12:04:39 +0200

    [UPDATE]: Added Comments.

--- AsyncTaskExamples/src/com/rtrk/asynctask/AsyncTaskExamplesActivity.java
+++ AsyncTaskExamples/src/com/rtrk/asynctask/AsyncTaskExamplesActivity.java
@@ -11,122 +11,125 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class AsyncTaskExamplesActivity extends Activity {
-	ProgressBar pBar;
-	Handler handler;
-	
-	
+    /** Fields. */
+    private ProgressBar mBar = null;;
+    private Handler mHandler = null;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        
-        pBar = (ProgressBar)findViewById(R.id.progressBar1);
-        handler = new Handler();
-        
-        Button btnThread = (Button)findViewById(R.id.btnThread);
+        mBar = (ProgressBar) findViewById(R.id.progressBar);
+        /** Handler has to be defined outside thread, not it thread! */
+        mHandler = new Handler();
+        /** Get Button Instance. */
+        Button btnThread = (Button) findViewById(R.id.btnThread);
+        /** Add ClickListener. */
         btnThread.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				threadDemo();
-			}
-
-		});
-
-        Button btnAsync = (Button)findViewById(R.id.btnAsync);
+            @Override
+            public void onClick(View v) {
+                threadDemo();
+            }
+        });
+        Button btnAsync = (Button) findViewById(R.id.btnAsync);
         btnAsync.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				asyncDemo();
-			}
-
-		});
-        
+            @Override
+            public void onClick(View v) {
+                asyncDemo();
+            }
+        });
     }
-    
-	private void threadDemo() {
-		Thread t = new Thread() {
-			int count = 0;
-			public void run() {
-				count = 0;
-				updateUI();
-				
-				for (int i = 1; i <= 5; i++) {
-					count = i * 20;
-					// update UI
-					updateUI();
-					// sleep
-					try { sleep(1000); } catch (InterruptedException e) { e.printStackTrace();}
-				}
-				toast();
-			}
-			private void updateUI() {
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						pBar.setProgress(count);
-					}
-				});
-			}
 
-			private void toast() {
-				handler.post(new Runnable() {
-					@Override
-					public void run() {
-						Toast.makeText(getApplicationContext(), "Job completed", Toast.LENGTH_LONG).show();
-					}
-				});
-			}
+    /** Create and Start Thread. */
+    private void threadDemo() {
+        Thread lThread = new Thread() {
+            int mThreadCount = 0;
 
-		};
-		t.start();
-	}
-	
-	private void asyncDemo() {
-		ProgressBarTask task = new ProgressBarTask();
-		task.execute(1);
-	}
+            public void run() {
+                mThreadCount = 0;
+                updateUI();
+                for (int i = 1; i <= 5; i++) {
+                    mThreadCount = i * 20;
+                    /** Update UI. */
+                    updateUI();
+                    /** Sleep a second. */
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                toast();
+            }
 
+            /** Update UI. */
+            private void updateUI() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        /** Update Progress Bar. */
+                        mBar.setProgress(mThreadCount);
+                    }
+                });
+            }
 
-	/** <InitParameterType, ProgressType, ResultType>
-	 * 
-	 * @author minja
-	 *
-	 */
-	class ProgressBarTask extends AsyncTask<Integer, Integer, Integer> {
-		int count = 0;
-		
-		@Override
-		protected Integer doInBackground(Integer... params) {
-			count = 0;
-			publishProgress(count);
+            /** Show Toast. */
+            private void toast() {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Job completed", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        };
+        lThread.start();
+    }
 
-			int start = params[0];
-			for (int i = start; i <= 5; i++) {
-				count = i * 20;
-				// call onProgressUpdate, which will updateUI
-				publishProgress(count);
-				// sleep
-				try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace();}
-			}
-			// the value that we return here will go to the onPostExecute() as an argument
-			return count;
-		}
-		
-		@Override
-		protected void onProgressUpdate(Integer ... value) {
-			int current = value[0];
-			pBar.setProgress(current);
-		}
-		
-		@Override
-		protected void onPostExecute(Integer result) {
-			Toast.makeText(getApplicationContext(), "Job completed: " + result, Toast.LENGTH_LONG).show();
-		}
+    private void asyncDemo() {
+        ProgressBarTask task = new ProgressBarTask();
+        task.execute(1);
+    }
 
-	}	
-	
+    /** Update UI with AsyncTask. */
+    private class ProgressBarTask extends AsyncTask<Integer, Integer, Integer> {
+        int mAsyncCount = 0;
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            mAsyncCount = 0;
+            publishProgress(mAsyncCount);
+            int start = params[0];
+            for (int i = start; i <= 5; i++) {
+                mAsyncCount = i * 20;
+                /** Call onProgressUpdate, which will updateUI. */
+                publishProgress(mAsyncCount);
+                /** Sleep. */
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            /**
+             * The value that we return here will go to the onPostExecute() as
+             * an argument.
+             */
+            return mAsyncCount;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... value) {
+            int current = value[0];
+            mBar.setProgress(current);
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            Toast.makeText(getApplicationContext(), "Job completed: " + result,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 }
